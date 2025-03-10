d309d1a6ede22c927d1ca28d0168c77f48a7204c
Elijah Moore <elijawhan@icloud.com>
2025-03-10 11:21:57 -0500

    Begin setup of validation

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 
+if [ $# -ne ]; then
+  echo "No Args required."
+  exit 1
+fi
+
+# Check if the virtual environment already exists
+if [ -d ".venv" ]; then
+  echo "Virtual environment '.venv' already exists."
+  echo "Activating..."
+fi
+
+# Create the virtual environment
+python3 -m venv ".venv"
+
+echo "Virtual environment '.venv' created successfully."
+
+# Activate the virtual environment (optional)
+source ".venv/bin/activate"
+
+echo "Virtual environment activated."
+
+echo "Installing dependencies."
+# TODO: For some reason this does not work
+python3 -m pip install -r requirements.txt
+
+
 SCRIPT_DIR=$(dirname "$0")
 cd "$SCRIPT_DIR" || exit
 
 cmake -S . -B build
 cd build
 make
+
+cd ../validation
