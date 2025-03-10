fd8c1c8a027d43cd6ac8dd7bdb79b91f3d47a2a4
Andrew <cag0050@DESKTOP-5BFBBBD.>
2025-03-10 11:20:45 -0500

    Added DFT host implementation and python print

--- src/convexa.h
+++ src/convexa.h
@@ -1,10 +1,18 @@
 #ifndef CONVEXA_H
 #define CONVEXA_H
 
-
 #include <vector>
-std::vector<double> host_convolve(const std::vector<double>& signal, const std::vector<double>& kernel);
-std::vector<double> host_dft(const std::vector<double> &signal);
+#include <complex>
+#include <cmath>
+#include <math.h>
+namespace ConvExa
+{
+constexpr std::complex<double> j(0.0, 1.0);
+constexpr double pi = 3.141592653589793238462643383279;
+constexpr float pi_f = 3.141592653589793238462643383279f;
+} //End namespace ConvExa
 
+std::vector<double> host_convolve(const std::vector<double>& signal, const std::vector<double>& kernel);
+std::vector<std::complex<double>> host_dft(const std::vector<double> &signal);
 
 #endif
--- src/dfts/host_dft.cpp
+++ src/dfts/host_dft.cpp
@@ -1,7 +1,22 @@
 #include "../convexa.h"
 
-#include <vector>
-std::vector<double> host_dft(const std::vector<double> &signal)
+std::vector<std::complex<double>> host_dft(const std::vector<double> &signal)
 {
-    return signal;
+    size_t N = signal.size();
+    std::vector<std::complex<double>> result;
+    result.reserve(N);
+    
+    for (int idx = 0; idx < static_cast<int>(N); idx++)
+    {
+        result.push_back({0.0, 0.0});
+        for (int jdx = 0; jdx < static_cast<int>(N); jdx++)
+        {
+            std::complex<double> exponential = std::exp(
+                -(ConvExa::j * 2.0 * ConvExa::pi * static_cast<double>(idx) * static_cast<double>(jdx) 
+                / static_cast<double>(N))    
+            );
+            result.back() += static_cast<std::complex<double>>(signal[jdx]) * exponential;
+        }
+    }
+    return result;
 }
