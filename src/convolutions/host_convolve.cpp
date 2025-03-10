193d209c1ddc409fe19c75ebe7e98e490f4bc0a7
Elijah Moore <elijawhan@icloud.com>
2025-03-10 10:55:42 -0500

    Implement host-side convolution

--- src/convolutions/host_convolve.cpp
+++ src/convolutions/host_convolve.cpp
@@ -1,6 +1,21 @@
 #include "../convexa.h"
-#include <vector>
-std::vector<double> host_convolve(const std::vector<double>& signal, const std::vector<double>& kernel) {
-    return signal;
-}
+std::vector<double> host_convolve(const std::vector<double> &signal, const std::vector<double> &kernel)
+{
+    size_t ol = signal.size() + kernel.size() - 1;
+    std::vector<double> result;
+    result.reserve(ol);
 
+    for (size_t n = 0; n < ol; n++)
+    {
+        // for each output location
+        double sum = 0;
+        for (size_t i = kernel.size(); i-- > 0;)
+        {
+            if (n >= i)
+                sum += kernel[i] * signal[n - i];
+        }
+        result.push_back(sum);
+    }
+
+    return result;
+}
