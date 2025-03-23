d3805c2ea5d3333c2c1f1620debd68d904ad1b66
elijawhan <ecm0022@uah.edu>
2025-03-23 15:06:11 -0500

    Add conv impl

--- src/convolutions/device_convolve.cu
+++ src/convolutions/device_convolve.cu
@@ -1,4 +1,18 @@
-#include <cuda_runtime.h>
+#include <convexa.h>
+
 namespace CXKernels {
-    __global__ basic_convolve()
+    __global__ void basic_full_convolve(float*A, float*B, float*C, unsigned int aN, unsigned int bN, unsigned int cN) {
+        unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
+        for (int n = index; n < cN; n += blockDim.x * gridDim.x) // scuttles down the signal
+        {
+            float sum = 0.0f;
+            // Convert the pixel
+            for (int i = 0; i < bN; i += 1) // scuttles down the kernel
+            {
+                if (n >= i && (n-i) < signal.size())
+                sum += B[i] * A[n - i];
+            }
+            C[n] = sum;
+        }
+    }
 }
