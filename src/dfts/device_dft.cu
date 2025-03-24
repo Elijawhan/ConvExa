c8e0ea24ccaaca934a42df0f2ebfa4c4f3e73d96
ZaahirHossain <andrew.gould2003@gmail.com>
2025-03-24 09:49:01 -0500

    Device dft first draft

--- src/dfts/device_dft.cu
+++ src/dfts/device_dft.cu
@@ -1,9 +1,25 @@
 #include <convexa.h>
+#include <cuda_runtime.h>
+#include <cuda/std/complex>
 
 namespace CXKernels
 {
-__global__ void device_dft(cuda::std::complex<float>* signal)
+__global__ void device_dft(float* signal, uint32_t length, cuda::std::complex<float>* result)
 {
-    int index = blockDim.x*blockIdx.x + threadIdx.x;
+    for (uint32_t index = blockDim.x*blockIdx.x + threadIdx.x;
+         index < length;
+         index += blockDim.x * gridDim.x)
+    {
+        cuda::std::complex<float> sum(0.0, 0.0);
+        for (uint32_t i = 0; i < length; i++)
+        {
+            cuda::std::complex<float> exponential = cuda::std::expf(
+                -(ConvExa::j_cuda * 2.0 * ConvExa::pi * 
+                  double(index) * double(i) / double(length))
+            );
+            sum += exponential;
+        }
+        result[index] = sum;
+    }
 }
 }
