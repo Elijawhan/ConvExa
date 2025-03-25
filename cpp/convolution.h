f1b32c4823e30785a12af4472aa5ad2f039488d4
elijawhan <ecm0022@uah.edu>
2025-03-24 19:22:52 -0500

    Segment subproject main for convolution and dft

--- cpp/convolution.h
+++ cpp/convolution.h
@@ -1,5 +1,13 @@
 #include <convexa.h>
+#include "helper.h"
 
 void c_main() {
     printf("Hello from Convolve main!\n");
+    std::vector<double> myVec = {2, 1, 3, 5, 7, 6, 4};
+    std::vector<double> myKernel = {0.125, -0.125}; 
+
+    std::vector<double> myResult = ConvExa::host_convolve(myVec, myKernel);
+
+    HELP::print_vec(myResult);
+    
 }
---
+++ cpp/helper.h
@@ -0,0 +1,51 @@
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+namespace HELP
+{
+    int tokenize_csv(
+        char *fname,
+        std::vector<float> &matrix) // Must pass in vectors so that the memory stays alive.
+    {
+        std::ifstream in_file;
+        in_file.open(fname);
+
+        std::string line, token;
+        int row_count = 0, cell_count = 0;
+
+        if (!in_file.is_open())
+        {
+            return -2;
+        }
+        row_count = 0;
+        while (std::getline(in_file, line))
+        {
+            row_count++;
+            std::stringstream line_stream(line);
+            std::vector<float> row;
+            while (std::getline(line_stream, token, ','))
+            {
+                cell_count++;
+                if (token != "," && token != "\n")
+                    row.push_back(std::stof(token));
+                else
+                    std::cout << "BAD TOKEN\n";
+            }
+            matrix.insert(matrix.end(), row.begin(), row.end());
+        }
+        in_file.close();
+        if (row_count)
+            return row_count;
+        else
+            return 1;
+    }
+    template <typename T>
+    void print_vec(std::vector<T> vec) {
+        for (T i: vec) {
+            printf("%d ");
+        }
+        printf("\n");
+    }
+}
