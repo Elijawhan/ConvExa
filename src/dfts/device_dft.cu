a5baea2a02aaf2b464b6abf533a2e7786eb99629
ZaahirHossain <andrew.gould2003@gmail.com>
2025-03-24 19:22:03 -0500

    Added timing function for DFT

--- src/dfts/device_dft.cu
+++ src/dfts/device_dft.cu
@@ -23,12 +23,12 @@ __global__ void device_dft(const T* signal, const uint32_t length, cuda::std::co
         result[index] = sum;
     }
 }
+template __global__ void device_dft<float>(const float* signal, const uint32_t length, cuda::std::complex<float>* result);
+template __global__ void device_dft<double>(const double* signal, const uint32_t length, cuda::std::complex<double>* result);
 }
 
-namespace ConvExa
-{
 template< typename T = double >
-void device_dft(const T* signal, const uint32_t length, cuda::std::complex<T>* result)
+void ConvExa::device_dft(const T* signal, const uint32_t length, cuda::std::complex<T>* result)
 {
     dim3 num_threads = 32;
     dim3 num_blocks = (N + num_threads - 1) / num_threads;
@@ -37,4 +37,47 @@ void device_dft(const T* signal, const uint32_t length, cuda::std::complex<T>* r
         signal, length, result
     );
 }
+
+template< typename T = double >
+void CXTiming::device_dft(const std::vector<T> &signal, std::vector<std::complex<T>> result)
+{
+    float *device_a = nullptr;
+    float *device_c = nullptr;
+
+    size_t byte_size_sig = signal.size() * sizeof(T);
+    size_t byte_size_output = byte_size_sig;
+    result.reserve(signal.size());
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    checkCudaErrors(cudaMalloc(&device_a, byte_size_sig));
+    checkCudaErrors(cudaMemcpy(device_a, signal.data(), byte_size_sig, cudaMemcpyHostToDevice));
+
+    checkCudaErrors(cudaMalloc(&device_c, byte_size_output));
+
+    dim3 num_threads = 1024;
+    dim3 num_blocks = (N + num_threads.x - 1) / num_threads.x;
+    cudaEventRecord(start);
+    // Memory Loaded, Perform Computations...
+    CXKernels::device_dft<<<num_blocks, num_threads>>>(
+        signal, length, result
+    );
+    cudaEventRecord(stop);
+
+    // Finish Computations before this block
+    checkCudaErrors(cudaMemcpy(output.data(), device_c, byte_size_output, cudaMemcpyDeviceToHost));
+
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    checkCudaErrors(cudaFree(device_a));
+    checkCudaErrors(cudaFree(device_c));
+
+    return milliseconds;
 }
+
+template void CXTiming::device_dft(const std::vector<float> &signal, std::vector<std::complex<float>> result);
+template void CXTiming::device_dft(const std::vector<double> &signal, std::vector<std::complex<double>> result);
