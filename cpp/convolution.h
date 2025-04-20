92a5f1ccfaf2c3e0370250a15e0fafc9364d3a64
elijawhan <ecm0022@uah.edu>
2025-04-20 16:57:31 -0500

    Finish up FFT Kernel

--- cpp/convolution.h
+++ cpp/convolution.h
@@ -15,7 +15,7 @@ void c_main() {
     std::vector<CONV_TYPE_USED> fdata = HELP::vec_cast<int16_t, CONV_TYPE_USED>(data);
     // std::vector<CONV_TYPE_USED> fdata = std::vector<CONV_TYPE_USED>({ 1, 2, 3, 4, 5});
     // Filter Design software used: http://t-filter.engineerjs.com
-    // std::vector<CONV_TYPE_USED> myKernel = {6, 7, 8, 9, 10, 11, 12, 13};
+    // std::vector<CONV_TYPE_USED> myKernel = {6, 7, 8};
     std::vector<CONV_TYPE_USED> myKernel = {0.005184042182695352, 0.00048479825762196997, 0.0005061197474262169, 0.0005270715548515555, 0.0005475890126449697,
                                    0.0005676036724039025, 0.0005870631509281676, 0.0006059009136524695, 0.000624040883389753, 0.0006413492301257154, 0.000657734482648827,
                                    0.0006731593746308583, 0.0006875614962714815, 0.0007008126483952237, 0.0007127726885099106, 0.0007234364085612756, 0.0007327589621457485,
@@ -100,6 +100,18 @@ void c_main() {
     else
         printf("FAIL\n");
 
+
+    myResult_d.clear();
+    float base_convolve_d_fft_timing = CXTiming::device_convolve_fft(fdata, myKernel, myResult_d);
+
+    re = HELP::relative_error(myResult_d, myResult_h);
+    printf("Relative Error device fft: %.8Lf ", re);
+    if (re < HELP::MAX_RELATIVE_ERROR_FLOAT)
+        printf("PASS\n");
+    else
+        printf("FAIL\n");
+
+
     myResult_d.clear();
     float base_convolve_d_oa_timing = CXTiming::device_convolve_overlap_save(fdata, myKernel, myResult_d);
 
@@ -110,18 +122,50 @@ void c_main() {
     else
         printf("FAIL\n");
 
+
+    //PERFORMANCE!!!!
+    // base_convolve_h_timing = 0;
+    // for (int i = 0; i < numRuns; i++)
+    // {
+    //     myResult_h.clear();
+    //     base_convolve_h_timing += CXTiming::host_convolve(fdata, myKernel, myResult_h);
+    // }
+    // base_convolve_h_timing = base_convolve_h_timing / numRuns;
+    base_convolve_d_timing = 0;
+    for (int i = 0; i < numRuns; i++)
+    {
         myResult_d.clear();
-    // float base_convolve_d_oa_timing = CXTiming::device_convolve_overlap_save(fdata, myKernel, myResult_d);
+        base_convolve_d_timing += CXTiming::device_convolve(fdata, myKernel, myResult_d);
+    }
+    base_convolve_d_timing = base_convolve_d_timing / numRuns;
 
-    // re = HELP::relative_error(myResult_d, myResult_h);
-    // printf("Relative Error device overlap: %.8Lf ", re);
-    // if (re < HELP::MAX_RELATIVE_ERROR_FLOAT)
-    //     printf("PASS\n");
-    // else
-    //     printf("FAIL\n");
+    base_convolve_d_oa_timing = 0;
+    for (int i = 0; i < numRuns; i++)
+    {
+        myResult_d.clear();
+        base_convolve_d_oa_timing += CXTiming::device_convolve_overlap_save(fdata, myKernel, myResult_d);
+    }
+    base_convolve_d_oa_timing = base_convolve_d_oa_timing / numRuns;
 
+    base_convolve_d_fft_timing = 0;
+    for (int i = 0; i < numRuns; i++)
+    {
+        myResult_d.clear();
+        base_convolve_d_fft_timing += CXTiming::device_convolve_fft(fdata, myKernel, myResult_d);
+    }
+    base_convolve_d_fft_timing = base_convolve_d_fft_timing / numRuns;
 
-    // //PERFORMANCE!!!!
+    printf("==== Timing for %d points over %d runs ====\n", data.size() + myKernel.size() - 1, numRuns);
+    // printf("Host Impl Timing: %f ms\n", base_convolve_h_timing);
+    printf("Basic Convolution Kernel Timing: %f ms\n", base_convolve_d_timing);
+    printf("Overlap Add Convolution Kernel Timing: %f ms\n", base_convolve_d_oa_timing);
+    printf("FFT Convolution Kernel Timing: %f ms\n", base_convolve_d_fft_timing);
+    HELP::write_wav("./cpp/audio/badadeedu.wav", HELP::vec_cast<CONV_TYPE_USED, int16_t>(myResult_d), hdr);
+
+    ////// SECOND ////
+    data.clear();
+    hdr = HELP::read_wav("./cpp/audio/classic_jam.wav", &data);
+    fdata = HELP::vec_cast<int16_t, float>(data);
     // base_convolve_h_timing = 0;
     // for (int i = 0; i < numRuns; i++)
     // {
@@ -129,32 +173,41 @@ void c_main() {
     //     base_convolve_h_timing += CXTiming::host_convolve(fdata, myKernel, myResult_h);
     // }
     // base_convolve_h_timing = base_convolve_h_timing / numRuns;
-    // base_convolve_d_timing = 0;
-    // for (int i = 0; i < numRuns; i++)
-    // {
-    //     myResult_d.clear();
-    //     base_convolve_d_timing += CXTiming::device_convolve(fdata, myKernel, myResult_d);
-    // }
-    // base_convolve_d_timing = base_convolve_d_timing / numRuns;
+    base_convolve_d_timing = 0;
+    for (int i = 0; i < numRuns; i++)
+    {
+        myResult_d.clear();
+        base_convolve_d_timing += CXTiming::device_convolve(fdata, myKernel, myResult_d);
+    }
+    base_convolve_d_timing = base_convolve_d_timing / numRuns;
 
-    // base_convolve_d_oa_timing = 0;
-    // for (int i = 0; i < numRuns; i++)
-    // {
-    //     myResult_d.clear();
-    //     base_convolve_d_oa_timing += CXTiming::device_convolve_overlap_save(fdata, myKernel, myResult_d);
-    // }
-    // base_convolve_d_oa_timing = base_convolve_d_oa_timing / numRuns;
+    base_convolve_d_oa_timing = 0;
+    for (int i = 0; i < numRuns; i++)
+    {
+        myResult_d.clear();
+        base_convolve_d_oa_timing += CXTiming::device_convolve_overlap_save(fdata, myKernel, myResult_d);
+    }
+    base_convolve_d_oa_timing = base_convolve_d_oa_timing / numRuns;
 
-    // printf("==== Timing for %d points over %d runs ====\n", data.size() + myKernel.size() - 1, numRuns);
+    base_convolve_d_fft_timing = 0;
+    for (int i = 0; i < numRuns; i++)
+    {
+        myResult_d.clear();
+        base_convolve_d_fft_timing += CXTiming::device_convolve_fft(fdata, myKernel, myResult_d);
+    }
+    base_convolve_d_fft_timing = base_convolve_d_fft_timing / numRuns;
+
+    printf("==== Timing for %d points over %d runs ====\n", data.size() + myKernel.size() - 1, numRuns);
     // printf("Host Impl Timing: %f ms\n", base_convolve_h_timing);
-    // printf("Basic Convolution Kernel Timing: %f ms\n", base_convolve_d_timing);
-    // printf("Overlap Add Convolution Kernel Timing: %f ms\n", base_convolve_d_oa_timing);
-    // HELP::write_wav("./cpp/audio/badadeedu.wav", HELP::vec_cast<CONV_TYPE_USED, int16_t>(myResult_d), hdr);
+    printf("Basic Convolution Kernel Timing: %f ms\n", base_convolve_d_timing);
+    printf("Overlap Add Convolution Kernel Timing: %f ms\n", base_convolve_d_oa_timing);
+    printf("FFT Convolution Kernel Timing: %f ms\n", base_convolve_d_fft_timing);
+    HELP::write_wav("./cpp/audio/unclassic_jam.wav", HELP::vec_cast<float, int16_t>(myResult_d), hdr);
 
-    // ////// THIRD ////
-    // data.clear();
-    // hdr = HELP::read_wav("./cpp/audio/classic_jam.wav", &data);
-    // fdata = HELP::vec_cast<int16_t, float>(data);
+    ////// THIRD ////
+    data.clear();
+    hdr = HELP::read_wav("./cpp/audio/subroutines.wav", &data);
+    fdata = HELP::vec_cast<int16_t, float>(data);
     // base_convolve_h_timing = 0;
     // for (int i = 0; i < numRuns; i++)
     // {
@@ -162,58 +215,34 @@ void c_main() {
     //     base_convolve_h_timing += CXTiming::host_convolve(fdata, myKernel, myResult_h);
     // }
     // base_convolve_h_timing = base_convolve_h_timing / numRuns;
-    // base_convolve_d_timing = 0;
-    // for (int i = 0; i < numRuns; i++)
-    // {
-    //     myResult_d.clear();
-    //     base_convolve_d_timing += CXTiming::device_convolve(fdata, myKernel, myResult_d);
-    // }
-    // base_convolve_d_timing = base_convolve_d_timing / numRuns;
+    base_convolve_d_timing = 0;
+    for (int i = 0; i < numRuns; i++)
+    {
+        myResult_d.clear();
+        base_convolve_d_timing += CXTiming::device_convolve(fdata, myKernel, myResult_d);
+    }
+    base_convolve_d_timing = base_convolve_d_timing / numRuns;
 
-    // base_convolve_d_oa_timing = 0;
-    // for (int i = 0; i < numRuns; i++)
-    // {
-    //     myResult_d.clear();
-    //     base_convolve_d_oa_timing += CXTiming::device_convolve_overlap_save(fdata, myKernel, myResult_d);
-    // }
-    // base_convolve_d_oa_timing = base_convolve_d_oa_timing / numRuns;
+    base_convolve_d_oa_timing = 0;
+    for (int i = 0; i < numRuns; i++)
+    {
+        myResult_d.clear();
+        base_convolve_d_oa_timing += CXTiming::device_convolve_overlap_save(fdata, myKernel, myResult_d);
+    }
+    base_convolve_d_oa_timing = base_convolve_d_oa_timing / numRuns;
 
-    // printf("==== Timing for %d points over %d runs ====\n", data.size() + myKernel.size() - 1, numRuns);
+    base_convolve_d_fft_timing = 0;
+    for (int i = 0; i < numRuns; i++)
+    {
+        myResult_d.clear();
+        base_convolve_d_fft_timing += CXTiming::device_convolve_fft(fdata, myKernel, myResult_d);
+    }
+    base_convolve_d_fft_timing = base_convolve_d_fft_timing / numRuns;
+
+    printf("==== Timing for %d points over %d runs ====\n", (data.size() + myKernel.size() - 1), numRuns);
     // printf("Host Impl Timing: %f ms\n", base_convolve_h_timing);
-    // printf("Basic Convolution Kernel Timing: %f ms\n", base_convolve_d_timing);
-    // printf("Overlap Add Convolution Kernel Timing: %f ms\n", base_convolve_d_oa_timing);
-    // HELP::write_wav("./cpp/audio/unclassic_jam.wav", HELP::vec_cast<float, int16_t>(myResult_d), hdr);
-
-    // ////// SECOND ////
-    // data.clear();
-    // hdr = HELP::read_wav("./cpp/audio/subroutines.wav", &data);
-    // fdata = HELP::vec_cast<int16_t, float>(data);
-    // base_convolve_h_timing = 0;
-    // for (int i = 0; i < numRuns; i++)
-    // {
-    //     myResult_h.clear();
-    //     base_convolve_h_timing += CXTiming::host_convolve(fdata, myKernel, myResult_h);
-    // }
-    // base_convolve_h_timing = base_convolve_h_timing / numRuns;
-    // base_convolve_d_timing = 0;
-    // for (int i = 0; i < numRuns; i++)
-    // {
-    //     myResult_d.clear();
-    //     base_convolve_d_timing += CXTiming::device_convolve(fdata, myKernel, myResult_d);
-    // }
-    // base_convolve_d_timing = base_convolve_d_timing / numRuns;
-
-    // base_convolve_d_oa_timing = 0;
-    // for (int i = 0; i < numRuns; i++)
-    // {
-    //     myResult_d.clear();
-    //     base_convolve_d_oa_timing += CXTiming::device_convolve_overlap_save(fdata, myKernel, myResult_d);
-    // }
-    // base_convolve_d_oa_timing = base_convolve_d_oa_timing / numRuns;
-
-    // printf("==== Timing for %d points over %d runs ====\n", (data.size() + myKernel.size() - 1), numRuns);
-    // printf("Host Impl Timing: %f ms\n", base_convolve_h_timing);
-    // printf("Basic Convolution Kernel Timing: %f ms\n", base_convolve_d_timing);
-    // printf("Overlap Add Convolution Kernel Timing: %f ms\n", base_convolve_d_oa_timing);
-    // HELP::write_wav("./cpp/audio/routines.wav", HELP::vec_cast<float, int16_t>(myResult_d), hdr);
+    printf("Basic Convolution Kernel Timing: %f ms\n", base_convolve_d_timing);
+    printf("Overlap Add Convolution Kernel Timing: %f ms\n", base_convolve_d_oa_timing);
+    printf("FFT Convolution Kernel Timing: %f ms\n", base_convolve_d_fft_timing);
+    HELP::write_wav("./cpp/audio/routines.wav", HELP::vec_cast<float, int16_t>(myResult_d), hdr);
 }
--- src/convolutions/device_convolve_fft.cu
+++ src/convolutions/device_convolve_fft.cu
@@ -6,72 +6,120 @@
 #include <cufft.h>
 #include <helper.h>
 
+// Macro for checking cuFFT errors
+#define CUFFT_CHECK(call) \
+    do { \
+        cufftResult err = call; \
+        if (err != CUFFT_SUCCESS) { \
+            std::cerr << "cuFFT Error: " << err << " at line " << __LINE__ << std::endl; \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0)
+
+
 #define BLOCK_LEN 1024
 namespace CXKernels
 {
-    template <typename T = double>
-    __global__ void vec_multiply(T *A, T *B, T *C, unsigned int N)
+    // Kernel for pointwise complex multiplication
+
+    __global__ void vec_multiply_complex_f(cufftComplex *A, cufftComplex *B, cufftComplex *C, unsigned int N)
     {
         unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
         for (int n = index; n < N; n += blockDim.x * gridDim.x) // scuttles down the signal
         {
-            C[n] = A[n] * B[n];
+            float ar = A[n].x;
+            float ai = A[n].y;
+            float br = B[n].x;
+            float bi = B[n].y;
+            // Complex multiplication: (ar + ai*i) * (br + bi*i) = (ar*br - ai*bi) + (ar*bi + ai*br)*i
+            C[n].x = ar * br - ai * bi;
+            C[n].y = ar * bi + ai * br;
         }
     }
 }
 template <typename T = double>
 float CXTiming::device_convolve_fft(const std::vector<T> &signal, const std::vector<T> &kernel, std::vector<T> &output)
 {
-    T *device_a = nullptr;
-    T *device_b = nullptr;
-    T *device_c = nullptr;
-    size_t byte_size_sig = signal.size() * sizeof(T);
-    size_t byte_size_kernel = kernel.size() * sizeof(T);
-    size_t ol = (signal.size() + kernel.size() - 1);
-    size_t byte_size_output = ol * sizeof(T);
-    output.resize(ol);
+    int output_size = signal.size() + kernel.size() -1;
+    output.resize(output_size);
+    int fft_size = 1;
+    // Choose size of FFT (Nearest Pow 2)
+    while (fft_size < output_size) fft_size <<= 1;
+    
+    T *d_signal, *d_kernel;
+    cufftComplex *d_signalFFT, *d_kernelFFT, *d_productFFT;
+    T *d_output;
+
+    // printf("%d size, len: %d\n", fft_size, signal.size());
+
+    checkCudaErrors(cudaMalloc(&d_signal, fft_size * sizeof(T)));
+    checkCudaErrors(cudaMalloc(&d_kernel, fft_size * sizeof(T)));
+    checkCudaErrors(cudaMalloc(&d_signalFFT, fft_size * sizeof(cufftComplex)));
+    checkCudaErrors(cudaMalloc(&d_kernelFFT, fft_size * sizeof(cufftComplex)));
+    checkCudaErrors(cudaMalloc(&d_productFFT, fft_size * sizeof(cufftComplex)));
+    checkCudaErrors(cudaMalloc(&d_output, fft_size * sizeof(T)));
+
+
+    // checkCudaErrors(cudaMemset(d_signal, 0, fft_size * sizeof(T))); // Zero Pad
+    checkCudaErrors(cudaMemcpy(d_signal, signal.data(), signal.size() * sizeof(T), cudaMemcpyHostToDevice));
+    
+    checkCudaErrors(cudaMemset(d_kernel, 0, fft_size * sizeof(T))); // Zero Pad
+    checkCudaErrors(cudaMemcpy(d_kernel, kernel.data(), kernel.size() * sizeof(T), cudaMemcpyHostToDevice));
+
+    // Create cuFFT plans
+    cufftHandle planForward, planInverse;
+    CUFFT_CHECK(cufftPlan1d(&planForward, fft_size, CUFFT_R2C, 1)); // Real-to-complex
+    CUFFT_CHECK(cufftPlan1d(&planInverse, fft_size, CUFFT_C2R, 1)); // Complex-to-real
+
 
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    checkCudaErrors(cudaMalloc(&device_a, byte_size_sig));
-    checkCudaErrors(cudaMemcpy(device_a, signal.data(), byte_size_sig, cudaMemcpyHostToDevice));
-
-    checkCudaErrors(cudaMalloc(&device_b, byte_size_kernel));
-    checkCudaErrors(cudaMemcpy(device_b, kernel.data(), byte_size_kernel, cudaMemcpyHostToDevice));
-
-    checkCudaErrors(cudaMalloc(&device_c, byte_size_output));
-    // We don't actually need to do any copying, as a matter of fact, that could result in stinky.
-    // checkCudaErrors(cudaMemcpy(device_c, C, byte_size, cudaMemcpyHostToDevice));
 
     dim3 blockSize(BLOCK_LEN);
-    if (ol < BLOCK_LEN)
-        blockSize.x = ol;
+    if (fft_size < BLOCK_LEN)
+        blockSize.x = fft_size;
     int blocks = signal.size() / blockSize.x + 1;
     dim3 gridSize(blocks);
     cudaEventRecord(start);
     // Memory Loaded, Perform Computations...
-    CXKernels::vec_multiply<T><<<gridSize, blockSize>>>(device_a, device_b, device_c, ol);
+    // Compute FFT of input
+    
+    CUFFT_CHECK(cufftExecR2C(planForward, d_signal, d_signalFFT));
+    // Compute FFT of kernel
+    CUFFT_CHECK(cufftExecR2C(planForward, d_kernel, d_kernelFFT));
 
+    CXKernels::vec_multiply_complex_f<<<gridSize, blockSize>>>(d_signalFFT, d_kernelFFT, d_productFFT, fft_size);
+
+    CUFFT_CHECK(cufftExecC2R(planInverse, d_productFFT, d_output));
+    
     cudaEventRecord(stop);
 
-    // Finish Computations before this block
-    checkCudaErrors(cudaMemcpy(output.data(), device_c, byte_size_output, cudaMemcpyDeviceToHost));
-    cudaGetLastError();
+    cudaMemcpy(output.data(), d_output, output_size * sizeof(T), cudaMemcpyDeviceToHost);
+
+    T scale = 1.0 / static_cast<T>(fft_size);
+    for (int i = 0; i < output.size(); ++i) {
+        output[i] *= scale;
+    }
 
     cudaEventSynchronize(stop);
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
 
-    checkCudaErrors(cudaFree(device_a));
-    checkCudaErrors(cudaFree(device_b));
-    checkCudaErrors(cudaFree(device_c));
+    CUFFT_CHECK(cufftDestroy(planForward));
+    CUFFT_CHECK(cufftDestroy(planInverse));
+    checkCudaErrors(cudaFree(d_signal));
+    checkCudaErrors(cudaFree(d_kernel));
+    checkCudaErrors(cudaFree(d_signalFFT));
+    checkCudaErrors(cudaFree(d_kernelFFT));
+    checkCudaErrors(cudaFree(d_productFFT));
+    checkCudaErrors(cudaFree(d_output));
 
     return milliseconds;
 }
 
-template float CXTiming::device_convolve_fft<double>(const std::vector<double> &, const std::vector<double> &, std::vector<double> &);
+// template float CXTiming::device_convolve_fft<double>(const std::vector<double> &, const std::vector<double> &, std::vector<double> &);
 template float CXTiming::device_convolve_fft<float>(const std::vector<float> &, const std::vector<float> &, std::vector<float> &);
 // template float CXTiming::device_convolve_overlap_add<uint16_t>(const std::vector<uint16_t>&, const std::vector<uint16_t>&, std::vector<uint16_t>&);
 // template float CXTiming::device_convolve_overlap_add<int16_t>(const std::vector<int16_t>&, const std::vector<int16_t>&, std::vector<int16_t>&);
